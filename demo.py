942ae045614de525fda5f9f7d67034fe54374ab8
TrickyVikram <vkumarsah999@gmail.com>
2024-07-23 00:06:37 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,15 @@
+import cv2
+
+image=cv2.imread("/Users/vikramkumar/Desktop/Ai_Ml_project/vikram.jpg")
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+cv2.imshow('Grayscale Image', gray_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
